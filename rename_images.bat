0bcc56d62b104b8236bbfa1bd520e092eeb97950
Administrator <teroak23@gmail.com>
2026-05-05 03:55:19 +0800

    shii

---
+++ rename_images.bat
@@ -0,0 +1,27 @@
+@echo off
+set "DRAWABLE=C:\Users\Administrator\AndroidStudioProjects\intprogXml\app\src\main\res\drawable"
+
+ren "%DRAWABLE%\Centrum Advance 100s.jpg" centrum_advance_100s.jpg
+ren "%DRAWABLE%\Coca-Cola Zero 1.5L.jpg" coca_cola_zero_1_5l.jpg
+ren "%DRAWABLE%\Corona Extra 355mL x 6.jpg" corona_extra_355ml_x_6.jpg
+ren "%DRAWABLE%\Del Monte Pineapple Juice 1.36L.webp" del_monte_pineapple_juice_1_36l.webp
+ren "%DRAWABLE%\Dyson V12 Detect Slim.png" dyson_v12_detect_slim.png
+ren "%DRAWABLE%\Heinz Tomato Ketchup 1.25kg.jpg" heinz_tomato_ketchup_1_25kg.jpg
+ren "%DRAWABLE%\Hershey's Crunch Choco Monaka.jpg" hersheys_crunch_choco_monaka.jpg
+ren "%DRAWABLE%\Johnnie Walker Black Label 1L.jpg" johnnie_walker_black_label_1l.jpg
+ren "%DRAWABLE%\Kirkland Almond Butter 765g.jpg" kirkland_almond_butter_765g.jpg
+ren "%DRAWABLE%\Kirkland Bath Tissue 30 rolls.jpg" kirkland_bath_tissue_30_rolls.jpg
+ren "%DRAWABLE%\L'Oreal Paris Revitalift Serum.png" loreal_paris_revitalift_serum.png
+ren "%DRAWABLE%\Lysol Disinfectant Spray.webp" lysol_disinfectant_spray.webp
+ren "%DRAWABLE%\Magnum Almond White.jpg" magnum_almond_white.jpg
+ren "%DRAWABLE%\Meiji Ess Cup Mini.jpg" meiji_ess_cup_mini.jpg
+ren "%DRAWABLE%\Moet & Chandon Brut Imperial.jpg" moet_chandon_brut_imperial.jpg
+ren "%DRAWABLE%\Nestle Vanilla Cone 6pcs.jpg" nestle_vanilla_cone_6pcs.jpg
+ren "%DRAWABLE%\Nissin Cup Noodles Seafood 5s.jpg" nissin_cup_noodles_seafood_5s.jpg
+ren "%DRAWABLE%\Quaker Oats Old Fashioned 1.2kg.jpg" quaker_oats_old_fashioned_1_2kg.jpg
+ren "%DRAWABLE%\Samsung 55 Crystal UHD 4K TV.jpg" samsung_55_crystal_uhd_4k_tv.jpg
+ren "%DRAWABLE%\Selecta Double Dutch 1.5L.avif" selecta_double_dutch_1_5l.avif
+ren "%DRAWABLE%\Seoju Melon Bar 8 x 75mL.jpg" seoju_melon_bar_8_x_75ml.jpg
+
+echo Done renaming images!
+pause
